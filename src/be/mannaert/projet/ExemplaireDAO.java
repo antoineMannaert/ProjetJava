55cb1129152117c6264cfc700e84dea651b2c150
'Antoine@LAPTOP-HR0CP521.(none) <antoine.mannaert@gmail.com>
2018-11-18 13:58:51 +0100

    ExemplaireDAO - mise en place de ExemplaireDAO

---
+++ src/be/mannaert/projet/ExemplaireDAO.java
@@ -0,0 +1,42 @@
+package be.mannaert.projet;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ExemplaireDAO extends DAO<Exemplaire>{
+	
+	public ExemplaireDAO(Connection conn) {
+		super(conn);
+	}
+	
+	public boolean create(Exemplaire ex){		
+		return false;
+	}
+	
+	public boolean delete(Exemplaire ex){
+		return false;
+	}
+	
+	public boolean update(Exemplaire ex){
+		return false;
+	}
+	
+	public Exemplaire find(int id){
+		
+		Exemplaire ex = new Exemplaire();
+		
+		try{
+			ResultSet result = this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM ExemplaireJeu WHERE idExemplaire = " + id);
+			if(result.first())
+				ex = new Exemplaire(id, result.getString("nomEx"), result.getBoolean("disponible"), result.getInt("idJeu"));
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		return ex;
+	}
+
+}
