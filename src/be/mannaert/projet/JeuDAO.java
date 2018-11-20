1bea665dadd28ad216f8075d7294f37e674b84a0
'Antoine@LAPTOP-HR0CP521.(none) <antoine.mannaert@gmail.com>
2018-11-20 20:25:28 +0100

    Mise en place de create, delete, update et correction find

--- src/be/mannaert/projet/JeuDAO.java
+++ src/be/mannaert/projet/JeuDAO.java
@@ -9,27 +9,61 @@ public class JeuDAO extends DAO<Jeu>{
 	}
 	
 	public boolean create(Jeu j){		
-		return false;
+		
+		try {
+			this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeUpdate("INSERT INTO Jeu (nomJeu, tarif, idConsole) VALUES (" + j.getNom() + ", " + j.getTarif() + ", " + j.getConsole().getIdConsole() + ");");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
 	public boolean delete(Jeu j){
-		return false;
+		
+		try {
+			this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeUpdate("DELETE FROM Jeu WHERE idJeu = " + j.getIdJeu() + ");");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
 	public boolean update(Jeu j){
-		return false;
+		
+		try {
+			this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeUpdate("UPDATE Jeu SET nomJeu = " + j.getNom() + ", tarif = " + j.getTarif() + ", idConsole = " + j.getConsole().getIdConsole() + " WHERE idJeu = " + j.getIdJeu() + ");");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
 	public Jeu find(int id){
 		
 		Jeu j = new Jeu();
+		Console c = new Console();
+		DAO<Console> cdao = new ConsoleDAO(ProjetConnection.getInstance());
 		
 		try{
 			ResultSet result = this.connect.createStatement(
 					ResultSet.TYPE_SCROLL_INSENSITIVE,
 					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Jeu WHERE idJeu = " + id);
-			if(result.first())
-				j = new Jeu(id, result.getString("nomJeu"), result.getInt("tarif"), result.getBoolean("disponible"));
+			if(result.first()) {
+				c = cdao.find(result.getInt("idConsole"));
+				j = new Jeu(id, result.getString("nomJeu"), result.getInt("tarif"), c);
+			}
 		}
 		catch(SQLException e){
 			e.printStackTrace();
