2f1f9a6ffdd3344653a256fb0b22feec35b71edf
'Antoine@LAPTOP-HR0CP521.(none) <antoine.mannaert@gmail.com>
2018-11-20 20:58:47 +0100

    Mise en place de create, delete, update et correction find

--- src/be/mannaert/projet/ReservationDAO.java
+++ src/be/mannaert/projet/ReservationDAO.java
@@ -10,28 +10,62 @@ public class ReservationDAO extends DAO<Reservation> {
 		super(conn);
 	}
 	
-	public boolean create(Reservation j){		
-		return false;
+	public boolean create(Reservation r){		
+		
+		try {
+			this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeUpdate("INSERT INTO Reservation (dateRes, etat, idJeu) VALUES (" + r.getDateRes() + ", " + r.getEtatRes() + ", " + r.getJeu().getIdJeu() + ");");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
-	public boolean delete(Reservation j){
-		return false;
+	public boolean delete(Reservation r){
+		
+		try {
+			this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeUpdate("DELETE FROM Reservation WHERE idReservation = " + r.getIdRes() + ");");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
-	public boolean update(Reservation j){
-		return false;
+	public boolean update(Reservation r){
+		
+		try {
+			this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeUpdate("UPDATE Reservation SET dateRes = " + r.getDateRes() + ", etat  = " + r.getEtatRes() + ", idJeu = " + r.getJeu().getIdJeu() +  " WHERE idRes = " + r.getIdRes() + ");");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
 	public Reservation find(int id){
 		
 		Reservation r = new Reservation();
+		Jeu j = new Jeu();
+		DAO<Jeu> jdao = new JeuDAO(ProjetConnection.getInstance());
 		
 		try{
 			ResultSet result = this.connect.createStatement(
 					ResultSet.TYPE_SCROLL_INSENSITIVE,
 					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Reservation WHERE idRes = " + id);
-			if(result.first())
-				r = new Reservation(id, result.getDate("dateRes"), result.getString("etat"), result.getInt("idJeu"), result.getInt("idUser"));
+			if(result.first()) {
+				j = jdao.find(result.getInt("idJeu"));
+				r = new Reservation(id, result.getDate("dateRes"), result.getString("etat"), j);
+			}
 		}
 		catch(SQLException e){
 			e.printStackTrace();
