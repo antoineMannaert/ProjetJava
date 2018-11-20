c55f633b34a7071fe9ba2b93c8d63a2fbe47052e
'Antoine@LAPTOP-HR0CP521.(none) <antoine.mannaert@gmail.com>
2018-11-20 16:25:43 +0100

    ConsoleDAO - mise en place du create, update et delete + màj du find

--- src/be/mannaert/projet/ConsoleDAO.java
+++ src/be/mannaert/projet/ConsoleDAO.java
@@ -10,16 +10,46 @@ public class ConsoleDAO extends DAO<Console> {
 		super(conn);
 	}
 	
-	public boolean create(Console c){		
-		return false;
+	public boolean create(Console c){	
+		
+		try {
+			this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeQuery("INSERT INTO Console (nomConsole, diminutif) VALUES (" + c.getNom() + ", " + c.getDiminutif() + ");");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
 	public boolean delete(Console c){
-		return false;
+		
+		try {
+			this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeQuery("DELETE FROM Console WHERE idConsole = " + c.getIdConsole() + ");");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
 	public boolean update(Console c){
-		return false;
+
+		try {
+			this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeQuery("UPDATE Console SET nomConsole = " + c.getNomConsole() + ", diminutif = " + c.getDiminutif() + ", anneeS WHERE idConsole = " + c.getIdConsole() + ");");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
 	public Console find(int id){
@@ -29,9 +59,16 @@ public class ConsoleDAO extends DAO<Console> {
 		try{
 			ResultSet result = this.connect.createStatement(
 					ResultSet.TYPE_SCROLL_INSENSITIVE,
-					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Console WHERE idJeu = " + id);
-			if(result.first())
-				c = new Console(id, result.getString("nomConsole"), result.getString("diminutif"), result.getInt("anneeSortie"), result.getInt("idJeu"));
+					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Console WHERE idConsole = " + id + ";");
+			if(result.first()) {
+				c = new Console(id, result.getString("nomConsole"), result.getString("diminutif"));
+			}
+			
+			result = this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Jeu WHERE idConsole = " + id + ";");
+			while(result.next())
+				c.AjouterListeJeux(/*...*/));
 		}
 		catch(SQLException e){
 			e.printStackTrace();
