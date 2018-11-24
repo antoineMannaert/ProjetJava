bcbc2b5ae5a7dd5abbcd19620ec456d86a2d831e
'Antoine@LAPTOP-HR0CP521.(none) <antoine.mannaert@gmail.com>
2018-11-24 15:08:41 +0100

    Mise en place de la suppression de compte

--- src/be/mannaert/projet/Menu.java
+++ src/be/mannaert/projet/Menu.java
@@ -1,12 +1,10 @@
 package be.mannaert.projet;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -25,7 +23,7 @@ public class Menu extends JFrame {
 	 */
 	public Menu(User u) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 550, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -43,11 +41,46 @@ public class Menu extends JFrame {
 				dispose();
 			}
 		});
-		btnDeco.setBounds(284, 210, 140, 40);
+		btnDeco.setBounds(374, 310, 150, 40);
 		contentPane.add(btnDeco);
 		
-		JLabel labelSolde = new JLabel("Solde" + u.getSolde());
+		JLabel labelSolde = new JLabel("Solde : " + u.getSolde());
 		labelSolde.setBounds(349, 11, 75, 20);
 		contentPane.add(labelSolde);
+		
+		JButton btnSupp = new JButton("Supprimer compte");
+		btnSupp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				UserDAO dao = new UserDAO(ProjetConnection.getInstance());
+				
+				try {
+					
+					if(dao.delete(u)) {
+						JOptionPane.showMessageDialog(null, "Suppression effectuée");
+						Login l = new Login();
+						l.setVisible(true);
+						dispose();
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Suppression non valide");
+					}
+				}
+						
+				catch(Exception err) {
+					System.out.println(err);
+				}
+			}
+		});
+		btnSupp.setBounds(214, 310, 150, 40);
+		contentPane.add(btnSupp);
+		
+		JButton btnChangerMDP = new JButton("Changer Mot de passe");
+		btnChangerMDP.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnChangerMDP.setBounds(54, 310, 150, 40);
+		contentPane.add(btnChangerMDP);
 	}
 }
