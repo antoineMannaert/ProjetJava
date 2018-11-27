ded739fb08bf2d4a7a5cb664651d8c3d550bd9ad
'Antoine@LAPTOP-HR0CP521.(none) <antoine.mannaert@gmail.com>
2018-11-27 23:51:14 +0100

    Retrait de imports inutiles

--- src/be/mannaert/projet/ListeExemplaires.java
+++ src/be/mannaert/projet/ListeExemplaires.java
@@ -1,15 +1,10 @@
 package be.mannaert.projet;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
-import javax.swing.ListModel;
-import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
